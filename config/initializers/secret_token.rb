f251995b69b754259caaf62af70f34d4b4a2d0c8
Jeff Parrish <the.parrish@gmail.com>
2014-04-19 16:24:21 -0700

    Initial commit of Rubymine generated rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = '8a5054e99dd531d89029390325a2ee874961b0e159eab0d544f943a308cbe86d0c641dea8c0bee2eaacfd8bd648a3614d3e400550ccd759378fd9ee3ed3cb71a'
